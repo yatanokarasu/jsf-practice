c6e54d59eafaedbbcb0006190d8fe025befe23ed
yatanokarasu <sync.outerphase@gmail.com>
2017-03-20 17:28:08 +0900

    Add JSF sample having panelGrid
    Signed-off-by: yatanokarasu <sync.outerphase@gmail.com>

---
+++ sample04/src/main/java/sample/MeiboBean.java
@@ -0,0 +1,70 @@
+/*
+ *  The MIT License (MIT)
+ *
+ *  Copyright (c) 2017 Yusuke TAKEI.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package sample;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ */
+@ManagedBean(name="meibo")
+public class MeiboBean {
+
+    private String name = "default";
+
+    private int number = 0;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    /**
+     * @param number the number to set
+     */
+    public void setNumber(final int number) {
+        this.number = number;
+    }
+
+    public String next() {
+        return "output.xhtml";
+    }
+
+}
